f9e897b71097b28173f915a381f2e97fc78cf480
danjford <forddj93@gmail.com>
2015-12-22 03:35:03 +0000

    The latest lodestar-ractive and es6-ifying the gh page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,120 +1,132 @@
-'use strict';
+(function (LodeRactive) { 'use strict';
 
-var base = '/lodestar-ractive';
+  LodeRactive = 'default' in LodeRactive ? LodeRactive['default'] : LodeRactive;
 
-var router = new LodeRactive({ DEBUG: false, useHistory: true, basePath: base });
+  function clearCloak() {
 
-function clearCloak() {
-
-  var elems = document.querySelectorAll('.cloak');
-  [].forEach.call(elems, function (el) {
-    el.classList.remove('cloak');
-  });
-}
-
-function scrollTo(element, to, duration) {
-  if (duration < 0) return;
-  var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
-
-  setTimeout(function () {
-    element.scrollTop = element.scrollTop + perTick;
-    if (element.scrollTop === to) return;
-    scrollTo(element, to, duration - 10);
-  }, 10);
-}
-
-router.createRoute({
-  path: '/tutorials',
-  controller: function controller() {},
-  view: {
-    el: '#main-page',
-    template: {
-      url: base + '/tutorials',
-      container: '#main-page',
-      notOnSame: true
-    }
-  }
-});
-
-router.createRoute({
-  path: '/load-example',
-  controller: function controller() {
-    var _this = this;
-
-    clearCloak();
-
-    setInterval(function () {
-      _this.set('time', new Date());
-    }, 1000);
-  },
-  view: {
-    el: '#examples',
-    template: {
-      url: base + '/load-example',
-      container: '#examples',
-      notOnSame: true
-    },
-    data: {
-      'time': new Date()
-    }
-  },
-  actions: {
-    downToFirst: function downToFirst() {
-      scrollTo(document.body, document.getElementById('first').offsetTop, 600);
-    }
-  }
-});
-
-router.createRoute({
-  path: '/',
-  controller: function controller() {
-    var _this2 = this;
-
-    clearCloak();
-
-    window.addEventListener("beforeunload", function () {
-      localStorage.indexData = JSON.stringify(_this2.get());
+    let elems = document.querySelectorAll('.cloak');
+    [].forEach.call(elems, function (el) {
+      el.classList.remove('cloak');
     });
-  },
-  view: {
-    el: '#main-page',
-    data: {},
-    data: localStorage.indexData ? JSON.parse(localStorage.indexData) : { 'todo': { 'items': [], 'max': 5 } },
-    template: {
-      url: base || '/',
-      container: '#main-page',
-      notOnSame: true
-    }
-  },
-  actions: {
-    randomColor: function randomColor() {
-      this.set('color', "#" + Math.random().toString(16).slice(2, 8));
-    },
-    downToFirst: function downToFirst() {
-      scrollTo(document.body, document.querySelectorAll('.panel')[0].offsetTop, 600);
-    },
-    addTodo: function addTodo(event, inputVal) {
-      if (inputVal.length && this.get('todo.items').length < this.get('todo.max')) {
-        this.push('todo.items', { task: inputVal });
-        this.set('todo.input', '');
-      }
-      event.original.preventDefault();
-    },
-    activeTab: function activeTab(event) {
-      var active = document.querySelectorAll('.active');
-
-      if (active.length) {
-        [].forEach.call(active, function (el) {
-          el.classList.remove('active');
-        });
-      }
-
-      document.querySelector(event.node.hash).classList.add('active');
-      event.node.classList.add('active');
-
-      event.original.preventDefault();
-      event.original.stopPropagation();
-    }
   }
-});
+
+  var TutorialsController = {
+
+    controller: function () {
+
+      clearCloak();
+
+      setInterval(() => {
+        this.set('time', new Date());
+      }, 1000);
+    }
+
+  };
+
+  var LoadExampleController = {
+
+    controller: function () {},
+
+    actions: {
+      downToFirst: function () {
+        scrollTo(document.body, document.getElementById('first').offsetTop, 600);
+      }
+    }
+
+  };
+
+  var IndexController = {
+
+    controller: function () {
+
+      clearCloak();
+
+      window.addEventListener("beforeunload", () => {
+        localStorage.indexData = JSON.stringify(this.get());
+      });
+    },
+
+    actions: {
+      randomColor: function () {
+        this.set('color', "#" + Math.random().toString(16).slice(2, 8));
+      },
+      downToFirst: () => {
+        scrollTo(document.body, document.querySelectorAll('.panel')[0].offsetTop, 600);
+      },
+      addTodo: function (event, inputVal) {
+        if (inputVal.length && this.get('todo.items').length < this.get('todo.max')) {
+          this.push('todo.items', { task: inputVal });
+          this.set('todo.input', '');
+        }
+        event.original.preventDefault();
+      },
+      activeTab: function (event) {
+        var active = document.querySelectorAll('.active');
+
+        if (active.length) {
+          [].forEach.call(active, function (el) {
+            el.classList.remove('active');
+          });
+        }
+
+        document.querySelector(event.node.hash).classList.add('active');
+        event.node.classList.add('active');
+
+        event.original.preventDefault();
+        event.original.stopPropagation();
+      }
+
+    }
+
+  };
+
+  let base = '/lodestar-ractive';
+
+  let router = new LodeRactive({ DEBUG: false, useHistory: true, basePath: base });
+
+  router.createRoute({
+    path: '/tutorials',
+    controller: TutorialsController,
+    view: {
+      el: '#main-page',
+      template: {
+        url: base + '/tutorials',
+        container: '#main-page',
+        notOnSame: true
+      }
+    }
+  });
+
+  router.createRoute({
+    path: '/load-example',
+    controller: LoadExampleController,
+    view: {
+      el: '#examples',
+      template: {
+        url: base + '/load-example',
+        container: '#examples',
+        notOnSame: true
+      },
+      data: {
+        'time': new Date()
+      }
+    }
+  });
+
+  router.createRoute({
+    path: '/',
+    controller: IndexController,
+    view: {
+      el: '#main-page',
+      data: {},
+      data: localStorage.indexData ? JSON.parse(localStorage.indexData) : { 'todo': { 'items': [], 'max': 5 } },
+      template: {
+        url: base || '/',
+        container: '#main-page',
+        notOnSame: true
+      }
+    }
+  });
+
+})(LodeRactive);
+//# sourceMappingURL=main.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
   rm = require('gulp-rimraf'),
   less = require('gulp-less'),
   path = require('path'),
-  babel = require('gulp-babel');
+  babel = require( 'rollup-plugin-babel' ),
+  rollup = require('gulp-rollup')
+  sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', function() {
   return gulp.src('assets').pipe(rm());
@@ -25,19 +27,28 @@ gulp.task('copy', function() {
       .pipe(gulp.dest('assets/images/'));
 });
 
-gulp.task('babel', function() {
-  return gulp.src('src/js/main.js')
-    .pipe(babel({
-      presets: ['es2015']
+gulp.task('rollup', function() {
+  return gulp.src('src/js/main.js', {read: false})
+    .pipe(rollup({
+      format: 'iife',
+      moduleName: 'main',
+      sourceMap: true,
+      plugins: [
+        babel()
+      ],
+      external: [ 'lodestar-ractive' ]
     }))
+    .pipe(sourcemaps.write(".")) // this only works if the sourceMap option is true
     .pipe(gulp.dest('assets/js/'));
 });
 
+
+
 gulp.task('watch', function() {
-  gulp.watch('src/js/**/*.js', [ 'babel' ]);
+  gulp.watch('src/js/**/*.js', [ 'rollup' ]);
   gulp.watch('src/less/**/*.less', [ 'less' ]);
 });
 
-gulp.task('default', ['copy', 'less', 'babel', 'watch']);
+gulp.task('default', ['copy', 'less', 'rollup', 'watch']);
 
-gulp.task('build', ['copy', 'less', 'babel']);
+gulp.task('build', ['copy', 'less', 'rollup']);
